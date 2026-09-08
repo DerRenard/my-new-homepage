99a99df67904e5bf4b0ac330d01bdc04e3dbb1c1
YS <derrenardl@gmail.com>
2026-09-08 15:35:11 +0400

    focus fix

--- scripts/search-float.js
+++ scripts/search-float.js
@@ -4,22 +4,26 @@ const searchFloatWindow = document.querySelector(".search-container");
 const activateSearchFloatWindow = document.querySelector(".search-box-form");
 
 const searchBoxContainer = document.querySelector(".search-box-container");
-const searchBarFloating = document.querySelector(".search-bar");
+const searchBarFloating = document.querySelector("#search-bar");
 
 const searchTextVisual = document.querySelector(".search-box-bar");
 
 activateSearchFloatWindow.addEventListener("click", (e) => {
-  searchBoxContainer.classList = "search-box-container search-box-container-none";
+  searchBoxContainer.classList =
+    "search-box-container search-box-container-none";
   searchFloatWindow.classList = "search-container search-container-big";
   overlay.classList = "overlay";
-  searchBarFloating.focus();
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      searchBarFloating.focus();
+    });
+  });
 });
 
 overlay.addEventListener("click", (e) => {
   searchTextVisual.textContent = searchBarFloating.value;
-  
+
   searchBoxContainer.classList = "search-box-container";
   searchFloatWindow.classList = "search-container search-container-none";
   overlay.classList = "overlay overlay-none";
 });
-
